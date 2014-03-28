d0b4b72e1ddf55f14df19609595e07e81b6c4fae
toby cabot <toby.cabot@affectiva.com>
2014-03-27 23:58:39 -0400

    downgrade grails22 to 2.2.4

--- grails22.rb
+++ grails22.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Grails22 < Formula
   homepage 'http://grails.org'
-  url 'http://dist.springframework.org.s3.amazonaws.com/release/GRAILS/grails-2.2.5.zip'
-  sha1 '370c502a2add74ca3ae2f15b959c8886c965c5ac'
+  url 'http://dist.springframework.org.s3.amazonaws.com/release/GRAILS/grails-2.2.4.zip'
+  sha1 'ac9568e71bd98d0459e97278bf56fce06627c23e'
 
   def install
     rm_f Dir["bin/*.bat", "bin/cygrails", "*.bat"]
